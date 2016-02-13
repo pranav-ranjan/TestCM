5a534172406711d53690da69d376b51f265b8328
Pranav Ranjan <pranav.ranjan@outlook.com>
2016-02-13 15:17:50 +0530

    none

--- core/server/config/env/core.server.config.env.development.js
+++ core/server/config/env/core.server.config.env.development.js
@@ -2,10 +2,4 @@ module.exports = {
 //Development configuration options
 db:"mongodb://localhost/customermanager",
 sessionSecret:"developmentSessionSecret",
-facebook: {
-  clientID: '108009839548110',
-  clientSecret: '33dd3e3493678efd2006071bfbdc3e26',
-  callbackURL: 'http://localhost:8085/oauth/facebook/callback',
-  profileFields: ['id', 'displayName', 'photos', 'email']
-}
 };
