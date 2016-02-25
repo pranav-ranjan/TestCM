6f8cda3c5614b79addafd015f1f097722912b5dc
Pranav Ranjan <pranav.ranjan@outlook.com>
2016-02-25 22:14:02 +0530

    grunt fix

--- scripts/install_server.sh
+++ scripts/install_server.sh
@@ -1,6 +1,6 @@
 cd /home/ec2-user/CustomerMgrAWS
 mongorestore dump
-mongorestore -d customermanager-test dump
+mongorestore -d customermanager-test dump/customermanager
 npm install grunt-forever --save-dev
 npm install
 bower install --allow-root
