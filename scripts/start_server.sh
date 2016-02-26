51afa418cff81781b985135b951b9903ef41ed98
Pranav Ranjan <pranav.ranjan@outlook.com>
2016-02-26 10:46:03 +0530

    grunt change

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,7 @@
 #export NODE_ENV=development
 cd /home/ec2-user/CustomerMgrAWS
 #forever start server.js
-grunt forever:server:start
+#grunt forever:server:start
+echo 'Before grunt'
+grunt
+echo 'After grunt'
